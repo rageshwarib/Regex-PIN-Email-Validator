45349f817916f852cf8a6f59ed99a103a862e0cd
rageshwari <ragu.bodke@gmail.com>
2020-03-26 21:00:16 +0530

    Restrict the PIN from taking alphabets or special characters at the last

--- regexPinValidator.sh
+++ regexPinValidator.sh
@@ -2,7 +2,7 @@
 echo "Welcome to PIN code validator problem"
 
 read -p "Enter six digit code of Post Office::" sixDigitPIN
-patternToValidatePIN="^[1-9][0-9]{5}$"
+patternToValidatePIN="^[1-9][0-9]{4}[0-9]$"
 
 if [[ $sixDigitPIN =~ $patternToValidatePIN ]]
 then
