0737ed69628dfa3f9b371a9862bf15e9469bc619
rageshwari <ragu.bodke@gmail.com>
2020-03-27 00:31:52 +0530

    Ensuring @ and validating the mandatory second part

--- regexEmailValidator.sh
+++ regexEmailValidator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 echo "Welcome to Email Validator problem"
 read -p "Enter your email id:" emailId
-patternToValidateEmailId="^[a-zA-Z]{1,}([.]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2,3})?$"
+patternToValidateEmailId="^[a-zA-Z]{1,}([.]?[a-zA-Z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2,3})?$"
 if [[ $emailId =~ $patternToValidateEmailId ]]
 then
 	echo Valid Email Id
