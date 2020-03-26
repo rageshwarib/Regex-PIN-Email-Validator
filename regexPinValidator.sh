f8b2784744f3878b510511c0df87def68ba41a7c
rageshwari <ragu.bodke@gmail.com>
2020-03-26 20:04:51 +0530

    Validate six digit PIN code of Post Office

--- regexPinValidator.sh
+++ regexPinValidator.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to PIN code validator problem"
+
+read -p "Enter six digit code of Post Office::" sixDigitPIN
+patternToValidatePIN="^[0-9]{6}$"
+
+if [[ $sixDigitPIN =~ $patternToValidatePIN ]]
+then
+	echo Valid PIN code
+else
+	echo Invalid PIN code
+fi
